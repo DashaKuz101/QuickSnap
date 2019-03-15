fe1472bed71dfbe45e804b70f9248c14f649b274
DashaKuz101 <4325745@students.swinburne.edu.my>
2019-03-16 01:57:39 +0800

    Play second sound

--- src/GameLogic/Snap.cs
+++ src/GameLogic/Snap.cs
@@ -152,6 +152,7 @@ namespace CardGames.GameLogic
 			else if (player >= 0 && player < _score.Length)
 			{
 				_score [player]--;
+				SwinGame.PlaySoundEffect ("Slap2");
 			}
 
 			// stop the game...
